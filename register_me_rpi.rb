eca775fe638c327df50414552f6b56b7256d22ed
celic <cceli@codequarry.net>
2014-11-03 17:24:27 -0500

    Adds parameters

--- register_me_rpi.rb
+++ register_me_rpi.rb
@@ -1,9 +1,17 @@
 require 'watir'
 
+# Gather parameters
+RIN = ARGV[0]
+PASSWORD = ARGV[1]
+CRNS = ARGV[2..-1]
+
+# Launch FireFox and navigate to login page
 browser = Watir::Browser.new :firefox
 browser.goto 'https://sis.rpi.edu/rss/twbkwbis.P_WWWLogin'
 
-browser.text_field(id: 'UserID').set 'RIN'
-browser.text_field(name: 'PIN').set 'PASSWORD'
-
+# Sign in with parameters
+browser.text_field(id: 'UserID').set RIN
+browser.text_field(name: 'PIN').set PASSWORD
 browser.button(value: 'Login').click
+
+# Navigate to register page
